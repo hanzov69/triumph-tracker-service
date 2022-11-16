aa6e685f307e75f1730a1443db6c76cf66de58d5
hanzov69 <hanzo@freezerpants.com>
2022-11-16 17:39:23 -0500

    docstrings, minor cleanup

--- docker/app/frontend/app.py
+++ docker/app/frontend/app.py
@@ -1,19 +1,21 @@
-import os.path, time
+import os.path
 import sqlite3
+import time
+
 from aiohttp import request
 from flask import Flask, render_template, request
 
-
-    
 app = Flask(__name__)
 
 def get_db_connection():
+    '''Set up sqlite3 connection'''
     conn = sqlite3.connect('../clan_data.sqlite3')
     conn.row_factory = sqlite3.Row
     return conn
 
 @app.route('/')
 def index():
+    '''flask route for default page'''
     conn = get_db_connection()
     raids = conn.execute('select * from raids').fetchall()
     conn.close()
@@ -21,6 +23,7 @@ def index():
 
 @app.route('/_is_complete', methods=['POST'])
 def is_complete():
+    '''used for JS AJAX to determine if if an achievement is complete'''
     cheevo_id = request.form.get('cheevoid')
     player_id = request.form.get('playerid')
     conn = get_db_connection()
@@ -28,12 +31,13 @@ def is_complete():
     if progress[0] == 1:
         progress = "Done"
     else:
-         progress = "Incomplete"
+        progress = "Incomplete"
     conn.close()
     return (progress)
 
 @app.route('/_cheevo_desc', methods=['POST'])
 def cheevo_desc():
+    '''used for JS AJAX to get cheevo description'''
     cheevo_id = request.form.get('cheevodescid')
     conn = get_db_connection()
     result = conn.execute('select desc from cheevos where id=%s' % (cheevo_id,)).fetchone()
@@ -42,19 +46,22 @@ def cheevo_desc():
 
 @app.route('/_manifest_version', methods=['POST'])
 def manifest_version():
+    '''used for JS AJAX to get the local bungie manifest version'''
     with open('../version.txt', 'r') as file:
-            manifest_version = file.read().rstrip()
-            file.close()
+        manifest_version = file.read().rstrip()
+        file.close()
     return manifest_version
 
 @app.route('/_modified_time', methods=['POST'])
 def modified_time():
+    '''used for JS AJAX to get timestamp of a file'''
     filename = request.form.get('filename')
     modified_stamp = ("%s" % time.ctime(os.path.getmtime('../' + filename)))
     return modified_stamp
 
 @app.route('/raid/<raid_id>')
 def raid(raid_id):
+    '''Route to generate raid table, takes raid_id as param'''
     conn = get_db_connection()
     seal_name = conn.execute("select seal from raids where id=%s" % (raid_id,)).fetchone()
     cheevos = conn.execute("select * from cheevos where raid=%s" % (raid_id,)).fetchall()
@@ -63,4 +70,5 @@ def raid(raid_id):
     return render_template('raid.html', seal_name=seal_name, cheevos=cheevos, players=players)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run()
+    
